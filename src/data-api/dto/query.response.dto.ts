f6bff1b10e8af0247e71dcd093db967a110c91ce
Scafu <alessandro.mazzariol04@gmail.com>
2026-03-20 13:11:04 +0100

    fix: added ApiProperty somewhere

--- src/data-api/dto/query.response.dto.ts
+++ src/data-api/dto/query.response.dto.ts
@@ -1,7 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { EncryptedEnvelopeDto } from './encrypted-envelope.dto';
 
 export class QueryResponseDto {
+  @ApiProperty({ type: [EncryptedEnvelopeDto] })
   data: EncryptedEnvelopeDto[];
+  @ApiProperty({ name: 'next_cursor' })
   nextCursor?: string;
+  @ApiProperty({ name: 'has_more' })
   hasMore: boolean;
 }
