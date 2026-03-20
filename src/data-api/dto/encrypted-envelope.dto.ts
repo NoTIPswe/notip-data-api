adb002bca758b4693e118900279b931c85e5b3f2
alessandrocontarini <alessandro.contarini21@gmail.com>
2026-03-20 17:36:33 +0100

    feat: fix response dto

--- src/data-api/dto/encrypted-envelope.dto.ts
+++ src/data-api/dto/encrypted-envelope.dto.ts
@@ -1,10 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class EncryptedEnvelopeDto {
+  @ApiProperty({ name: 'gateway_id' })
   gatewayId: string;
+  @ApiProperty({ name: 'sensor_id' })
   sensorId: string;
+  @ApiProperty({ name: 'sensor_type' })
   sensorType: string;
+  @ApiProperty({ name: 'timestamp' })
   timestamp: string;
+  @ApiProperty({ name: 'encrypted_data' })
   encryptedData: string;
+  @ApiProperty({ name: 'iv' })
   iv: string;
+  @ApiProperty({ name: 'auth_tag' })
   authTag: string;
+  @ApiProperty({ name: 'key_version', type: Number })
   keyVersion: number;
 }
